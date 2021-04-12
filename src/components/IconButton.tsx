2740b71e9f13b6b68a5dbd8dd9d89aa966b8e83f
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-04-12 20:00:29 +0200

    add video player

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,8 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import styles from '../styles/IconButton.module.scss';
 
-const IconButton: FunctionComponent<{ icon: IconProp }> = ({ icon }) => {
-    return <FontAwesomeIcon className={styles.button} icon={icon} />;
+const IconButton: FunctionComponent<{ icon: IconProp; onClick: Function }> = ({ icon, onClick }) => {
+    return <FontAwesomeIcon className={styles.button} icon={icon} onClick={() => onClick()} />;
 };
 
 export default IconButton;
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,21 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player/youtube';
+import { faExpand, faPause, faPlay, faStepBackward, faStepForward } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/VideoPlayer.module.scss';
 import IconButton from './IconButton';
-import { faExpand, faPause, faStepBackward, faStepForward } from '@fortawesome/free-solid-svg-icons';
 import ProgressBar from './ProgressBar';
 
 const VideoPlayer = () => {
+    const [player, setPlayer] = useState<ReactPlayer>();
+    const [playerCurrentTime, setPlayerCurrentTime] = useState<number>(0);
+    const [isPlayerPlaying, setIsPlayerPlaying] = useState<boolean>(false);
+
+    const onReady = (player: ReactPlayer) => {
+        setPlayer(player);
+        // setIsPlayerPlaying(true);
+    };
+
+    const onProgress = ({ playedSeconds }: { playedSeconds: number }) => {
+        setPlayerCurrentTime(playedSeconds);
+    };
+
+    const onPause = () => {
+        if (isPlayerPlaying) setIsPlayerPlaying(false);
+    };
+
+    const onPlay = () => {
+        if (!isPlayerPlaying) setIsPlayerPlaying(true);
+    };
+
+    const pauseVideo = (): void => {
+        setIsPlayerPlaying(false);
+    };
+
+    const playVideo = (): void => {
+        setIsPlayerPlaying(true);
+    };
+
+    const stepForward = (): void => {};
+    const stepBackward = (): void => {};
+
     return (
         <main className={styles.main}>
-            <iframe src={''} className={styles.video} />
+            <div className={styles.video}>
+                <ReactPlayer
+                    url={'https://www.youtube.com/watch?v=gA6ppby3JC8'}
+                    width={'100%'}
+                    height={'100%'}
+                    config={{ playerVars: {} }}
+                    controls={false}
+                    progressInterval={500}
+                    playing={isPlayerPlaying}
+                    // @ts-ignore
+                    onReady={onReady}
+                    onProgress={onProgress}
+                    onPlay={onPlay}
+                    onPause={onPause}
+                />
+            </div>
             <section className={styles.controls}>
                 <div className={styles.controlGroup}>
-                    <IconButton icon={faStepBackward} />
-                    <IconButton icon={faPause} />
-                    <IconButton icon={faStepForward} />
+                    <IconButton icon={faStepBackward} onClick={stepForward} />
+
+                    {isPlayerPlaying ? (
+                        <IconButton icon={faPause} onClick={pauseVideo} />
+                    ) : (
+                        <IconButton icon={faPlay} onClick={playVideo} />
+                    )}
+
+                    <IconButton icon={faStepForward} onClick={stepBackward} />
                 </div>
-                <ProgressBar secondsPassed={25} totalSeconds={205} />
-                <IconButton icon={faExpand} />
+                <ProgressBar
+                    secondsPassed={Math.floor(playerCurrentTime)}
+                    totalSeconds={Math.floor(player?.getDuration() ?? 1)}
+                />
+                <IconButton icon={faExpand} onClick={() => {}} />
             </section>
         </main>
     );
