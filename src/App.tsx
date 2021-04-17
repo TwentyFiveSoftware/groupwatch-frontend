ad3c40c97e9c1ce7eb4160b0d0c12808f9a1583c
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-04-17 16:33:17 +0200

    improve time formatting

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ import WatchPage from './components/WatchPage';
 export const socket = io(process.env['SOCKET_ENDPOINT'] ?? '');
 
 export const formatTime = (seconds: number) => {
-    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
     const sec = seconds % 60;
+
+    if (hours > 0)
+        return `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${sec < 10 ? '0' : ''}${sec}`;
+
     return `${minutes}:${sec < 10 ? '0' : ''}${sec}`;
 };
 
